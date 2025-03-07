fb75bf24ca8f2ff159785cc94a232b6dc00e361f
GutsofHyrule <Dresmanns@gmail.com>
2025-03-07 12:23:13 -0500

    update

--- models/booking.models.js
+++ models/booking.models.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const bookingSchema = new mongoose.Schema({
     email: { type: String, required: true },
-    date: { type: String, require: true},
+    date: { type: String, required: true},
     timeSlot: { type: String, required: true},
 }, {timestamps:true})
 
